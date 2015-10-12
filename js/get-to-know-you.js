fd777df210bfd33003da0fee11dbae89062652b2
vicfriedman <victoria.m.friedman@gmail.com>
2015-10-12 16:27:35 -0400

    solution

--- js/get-to-know-you.js
+++ js/get-to-know-you.js
@@ -1,1 +1,15 @@
 //Code Your Solution Here
+
+var name = prompt("What is your name?");
+
+var hometown = prompt("hey " + name + "Where are you from?");
+
+var age = prompt("How old are you?");
+
+var book = prompt("What's your favorite book?");
+
+var movie = prompt("What's your favorite movie?");
+
+var chocolate = prompt("If you had to give up chocolate or cheese for the rest of your life, which would you give up?");
+
+alert("Thank you for your application" + name + ". You are " + age + " from " + hometown + ". Your favorite movie is " + movie + " and your favorite book is " + book ". You would give up " chocolate + " for the rest of your life.")
